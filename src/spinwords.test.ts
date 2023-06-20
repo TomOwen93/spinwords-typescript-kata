d51506bd596e5a82cab740ee86b4e1e8fab8a622
TomOwen93 <mr.tsowen@gmail.com>
2023-06-20 13:59:33 +0100

    add spinwords kata working and test cases

--- src/spinwords.test.ts
+++ src/spinwords.test.ts
@@ -1,25 +1,21 @@
 import spinWords from "./spinwords";
 
 test("spinWords returns a string all five or more letter words reversed", () => {
-  expect(spinWords("Hey fellow warriors" )).toBe("Hey wollef sroirraw");
+  expect(spinWords("Hey fellow warriors")).toBe("Hey wollef sroirraw");
 });
 
 test("spinWords returns a string all five or more letter words reversed", () => {
-  expect(spinWords("This is a test" )).toBe("This is a test");
+  expect(spinWords("This is a test")).toBe("This is a test");
 });
 
 test("spinWords returns a string all five or more letter words reversed", () => {
-  expect(spinWords("This is another test" )).toBe("This is rehtona test");
+  expect(spinWords("This is another test")).toBe("This is rehtona test");
 });
 
 test("spinWords returns a string with a single word back, if one string is inputted. Reversed if input word contains 5 or more letters", () => {
-  expect(spinWords("test" )).toBe("test");
+  expect(spinWords("test")).toBe("test");
 });
 
 test("spinWords returns a string with a single word back, if one string is inputted. Reversed if input word contains 5 or more letters", () => {
-  expect(spinWords("Superduper" )).toBe("repudrepuS");
+  expect(spinWords("Superduper")).toBe("repudrepuS");
 });
-
-
-
-
--- src/spinwords.ts
+++ src/spinwords.ts
@@ -1,8 +1,10 @@
-function spinWords(inputString: string):string { 
-let splitString = inputString.split(" ")
-splitString = splitString.map(str => str.length > 5 ? str.split("").reverse().join("") : str)
+function spinWords(inputString: string): string {
+  let splitString = inputString.split(" ");
+  splitString = splitString.map((str) =>
+    str.length > 5 ? str.split("").reverse().join("") : str
+  );
 
-  return splitString.join(" ")
+  return splitString.join(" ");
 }
 
 export default spinWords;
